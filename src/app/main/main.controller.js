48004dea6d81702673560456a4dbf83dfe48b28b
Edson Pérez <Dema@Edsons-MBP.home>
2019-03-26 03:38:04 -0300

    update

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,10 +7,19 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController($scope,$rootScope,$timeout)
+    function MainController($scope,$rootScope)
     {
         var vm = this;
         vm.close= close;
+        $rootScope.share = share;
+
+        function share($event){
+            $event.stopPropagation();
+            var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + document.URL, 'facebook-popup', 'height=350,width=600');
+            if(facebookWindow.focus) { facebookWindow.focus(); }
+                return false;
+              
+        }
 
         function close(){
             console.log("close");
